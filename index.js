19b62050320182877ccec9c9b371214a8e119159
etamar <etamar.levy@gmail.com>
2021-03-20 19:26:23 -0400

    commit tree 724c7d59cd671fc156db527be3a564d5dfb1fe30

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers() {
-    const firstDrivers = function (drivers = ["Danny", "Etamar", "Tomer", "Niv", "Yotam"]){
-        return 
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier (multiplier) {
+    return function (fare) {
+        return multiplier * fare;
     }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, returnDrivers) {
+    return returnDrivers(arrayOfDrivers);
 }
