de564198c63c443fc2e58ef771de83e58da83213
etamar <etamar.levy@gmail.com>
2021-03-19 22:59:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers() {
+    const firstDrivers = function (drivers = ["Danny", "Etamar", "Tomer", "Niv", "Yotam"]){
+        return 
+    }
+}
